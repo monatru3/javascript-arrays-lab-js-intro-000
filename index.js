9a997a884e6942618c09459a00038b14e3ef0cc6
Simone D <monatru3@hotmail.com>
2018-01-30 07:42:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var kittens=["Milo", "Otis", "Garfield"];
 var destructivelyAppendKitten=function(name) {
   kittens.push(name)
   return kittens
-}
+};
 
 var destructivelyPrependKitten=function(name) {
   kittens.unshift(name)
   return kittens
-}
+};
 
 var destructivelyRemoveLastKitten= ()=> {
   kittens.pop()
   return kittens
-}
+};
