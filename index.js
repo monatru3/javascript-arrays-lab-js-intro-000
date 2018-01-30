62d6918fde1dd0fe3fa93abdc619758474cd0206
Simone D <monatru3@hotmail.com>
2018-01-30 07:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ var destructivelyAppendKitten=function(name) {
   kittens.push(name)
   return kittens
 }
+
+var destructivelyPrependKitten=function(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+var destructivelyRemoveLastKitten= ()=> {
+  kittens.pop()
+  return kittens
+}
